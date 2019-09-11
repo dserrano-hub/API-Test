22468487aa04cefeb0cb08be8543492510b8e7d3
dserrano-hub <dserrano670@hotmail.com>
2019-09-10 22:56:51 -0500

    implemet search

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var request = new XMLHttpRequest();
+/* Variables */
 
 const app = document.getElementById('root');
 const container = document.createElement('div');
@@ -6,32 +6,20 @@ container.setAttribute('class', 'container');
 
 app.appendChild(container);
 
+/* Events */
+
+document.getElementById('search-button').addEventListener('click', clicked);
+
 function clicked() {
-    var input_value = document.getElementById('data').value;
-    document.getElementById('display').innerHTML = input_value;
+    var input_value = document.getElementById('input-search').value;
+    //document.getElementById('display').innerHTML = input_value;
     console.log(input_value);
     searchLocation(input_value);
 }
 
-function myFunction() {
-    document.getElementById("myDropdown").classList.toggle("show");
-  }
-  
-  // Close the dropdown if the user clicks outside of it
-  window.onclick = function(event) {
-    if (!event.target.matches('.dropbtn')) {
-      var dropdowns = document.getElementsByClassName("dropdown-content");
-      var i;
-      for (i = 0; i < dropdowns.length; i++) {
-        var openDropdown = dropdowns[i];
-        if (openDropdown.classList.contains('show')) {
-          openDropdown.classList.remove('show');
-        }
-      }
-    }
-  }
-
 function searchLocation(location) {
+  
+var request = new XMLHttpRequest();
     const COORS = 'https://cors-anywhere.herokuapp.com/';
     const API = `${COORS}https://www.metaweather.com/api/location/search/?query=${location}`;
     request.open('GET', API);
@@ -41,15 +29,53 @@ function searchLocation(location) {
         var data = JSON.parse(this.response);
 
         data.forEach(location => {
-            const card = document.createElement('div');
+          if (request.status >= 200 && request.status < 400) {
+            /*data.forEach(location => {
+             
+              // Create a div with a card class
+              const card = document.createElement('div');
+              card.setAttribute('class', 'card');
+
+              // Create an h1 and set the text content to the film's title
+              const title = document.createElement('div');
+              title.textContent = location.title;
+
+              container.appendChild(card);
+              card.appendChild(title);
+             
+              //document.getElementsByClassName('card-title').innerHTML = location_title;
+              console.log(location);
+            });*/
+            for (let i = 0; i < data.length; i++) {
+              const card = document.createElement('div');
+              card.setAttribute('class', 'card');
+
+              // Create an h1 and set the text content to the film's title
+              const title = document.createElement('div');
+              title.textContent = i.title;
+
+              container.appendChild(card);
+              card.appendChild(title);
+             
+              //document.getElementsByClassName('card-title').innerHTML = location_title;
+              console.log(i);
+              
+            }
+          } else {
+            console.log('error');
+          }
+          /*const containerr = document.createElement('div');
+            containerr.setAttribute('class', 'containerr');
+           const card = document.createElement('div');
 
             const a = document.createElement('a');
             a.setAttribute("href", "#");
             a.setAttribute("onclick", `some(${location.id})`);
             a.textContent = location.title;
-
-            container.appendChild(card);
-            card.appendChild(a);
+            
+            app.appendChild(containerr);
+            containerr.appendChild(card);
+            card.appendChild(a);*/
 
 
         });
@@ -65,4 +91,3 @@ function searchLocation(location) {
     request.setRequestHeader("Accept", 'application/json');
     request.send();
 }
-document.getElementById('btn').addEventListener('click', clicked);
