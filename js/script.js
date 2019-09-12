6be30451994c4fd71db36c38792150d4444f1469
Daniela Serrano <dserrano@cecropiasolutions.com>
2019-09-12 15:40:34 -0500

    Local storage functionality

--- js/script.js
+++ js/script.js
@@ -45,38 +45,48 @@ function searchLocationInput() {
 
                 //Add to favorites
                 favorites_btn.onclick = function(e) {
-                    let locationId = location.woeid;
 
-                    // hacemos que no se ejecute el enlace
-                    e.preventDefault();
 
-                    // leemos los datos clave del producto y los guardamos en un objeto
-                    var datos = {
-                        id: document.getElementById("locationId")
+                    let fav_API = `${COORS}https://www.metaweather.com/api/location/${location.woeid}`;
+                    var request = new XMLHttpRequest();
+
+                    request.open('GET', fav_API, true);
+
+                    request.onload = function() {
+                        let locationId = location.woeid;
+
+                        // hacemos que no se ejecute el enlace
+                        e.preventDefault();
+
+                        // leemos los datos clave del producto y los guardamos en un objeto
+                        var datos = {
+                            id: document.getElementById("locationId")
+                        };
+
+                        // leemos los favoritos del localStorage
+                        var favoritos = localStorage.getItem("favoritos") || "[]";
+                        favoritos = JSON.parse(favoritos);
+
+                        // buscamos el producto en la lista de favoritos
+                        var posLista = favoritos.findIndex(function(e) { return e.id == datos.id; });
+                        if (posLista > -1) {
+                            // si está, lo quitamos
+                            favoritos.splice(posLista, 1);
+                        } else {
+                            // si no está, lo añadimos
+                            favoritos.push(datos);
+                        }
+
+                        // guardamos la lista de favoritos 
+                        localStorage.setItem("favoritos", JSON.stringify(favoritos));
+
+                        // leemos los favoritos del localStorage
+                        var favoritos = localStorage.getItem("favoritos") || "[]";
+                        favoritos = JSON.parse(favoritos);
+
                     };
-
-                    // leemos los favoritos del localStorage
-                    var favoritos = localStorage.getItem("favoritos") || "[]";
-                    favoritos = JSON.parse(favoritos);
-
-                    // buscamos el producto en la lista de favoritos
-                    var posLista = favoritos.findIndex(function(e) { return e.id == datos.id; });
-                    if (posLista > -1) {
-                        // si está, lo quitamos
-                        favoritos.splice(posLista, 1);
-                    } else {
-                        // si no está, lo añadimos
-                        favoritos.push(datos);
-                    }
-
-                    // guardamos la lista de favoritos 
-                    localStorage.setItem("favoritos", JSON.stringify(favoritos));
-
-                    // leemos los favoritos del localStorage
-                    var favoritos = localStorage.getItem("favoritos") || "[]";
-                    favoritos = JSON.parse(favoritos);
-
-                };
+                    request.send();
+                }
 
                 //function for detail information
 
